ef2b2b4ee2758098c2ac25dc86d69a0274a6b1a3
Sumukh Shivakumar <sumukhshiv@berkeley.edu>
2017-06-26 13:25:07 -0500

    Parsed all data

--- vzhack.py
+++ vzhack.py
@@ -2,20 +2,19 @@ import xlrd
 import csv
 
 def antenna_classify ():
-    # RESULT = ['apple','cherry','orange','pineapple','strawberry']
-    # RESULT1 = ['a', 'b', 'c', 'd', 'e']
     homes_path = '/Users/sumukhshivakumar/Desktop/vzhack/houseList.xlsx'
     antennas_path = '/Users/sumukhshivakumar/Desktop/vzhack/antennaLocations.xlsx'
 
     #Clears previous output
     open('output.csv', 'w').close()
 
-    # homes = open(list_of_homes, 'r')
-    # antennas = open(list_of_antenna_locations, 'r')
     homes_sheet = xlrd.open_workbook(homes_path).sheet_by_index(0)
     antennas_sheet = xlrd.open_workbook(antennas_path).sheet_by_index(0)
     print("houseList has " + str(homes_sheet.nrows) + " rows and " + str(homes_sheet.ncols) + " columns.")
     print("antennaLocations has " + str(antennas_sheet.nrows) + " rows and " + str(antennas_sheet.ncols) + " columns.")
+
+    for row in range(1, homes_sheet.nrows):
+            print("code: " + str(homes_sheet.cell_value(row, 0)) + " addr: " + str(homes_sheet.cell_value(row, 1)) + " LAT: " + str(homes_sheet.cell_value(row, 2)) + " LONG: " + str(homes_sheet.cell_value(row, 3)))
 
     with open("output.csv",'w') as resultFile:
         wr = csv.writer(resultFile, dialect='excel')
