2d53e15f2942873a48a0b5cdd81df553bb5af2e4
Sumukh Shivakumar <sumukhshiv@berkeley.edu>
2017-06-26 15:59:02 -0500

    Machine Learning working

--- vzhack.py
+++ vzhack.py
@@ -1,5 +1,10 @@
 import xlrd
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use("ggplot")
+from sklearn.cluster import KMeans
 
 def antenna_classify ():
     homes_path = 'houseList.xlsx'
@@ -15,6 +20,36 @@ def antenna_classify ():
 
     for row in range(1, homes_sheet.nrows):
             print("code: " + str(homes_sheet.cell_value(row, 0)) + " addr: " + str(homes_sheet.cell_value(row, 1)) + " LAT: " + str(homes_sheet.cell_value(row, 2)) + " LONG: " + str(homes_sheet.cell_value(row, 3)))
+
+
+    house_array = []
+
+    for row in range(1, homes_sheet.nrows):
+        array_tuple = [homes_sheet.cell_value(row, 2), homes_sheet.cell_value(row, 3)]
+        house_array.append(array_tuple)
+
+    # print(house_array)
+    # print(len(house_array))
+
+    X = np.array(house_array)
+    kmeans = KMeans(n_clusters=4)
+    kmeans.fit(X)
+
+    centroids = kmeans.cluster_centers_
+    labels = kmeans.labels_
+
+    print(centroids)
+    print(labels)
+
+
+    colors = ["g.","r.","c.","y."]
+
+    for i in range(len(X)):
+        print("coordinate:",X[i], "label:", labels[i])
+        plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize = 10)
+
+    plt.scatter(centroids[:, 0],centroids[:, 1], marker = "x", s=150, linewidths = 5, zorder = 10)
+    plt.show()
 
     with open("output.csv",'w') as resultFile:
         wr = csv.writer(resultFile, dialect='excel')
